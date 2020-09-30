ef2d79158c31e2eb7a0fdc7d359149d05cb56b10
Alexa-R <sasha600591@gmail.com>
2020-09-30 21:15:00 +0300

    delete parallel

--- src/test/java/runner/CucumberRunnerTest.java
+++ src/test/java/runner/CucumberRunnerTest.java
@@ -1,20 +1,13 @@
 package runner;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "stepdefs",
-        /*tags = "@firstTag",*/
-        plugin = {"pretty", "html:target/cucumber.html"},
-        snippets = CAMELCASE,
-        monochrome = true
+        glue = "stepdefs"
 )
 public class CucumberRunnerTest {
 }
